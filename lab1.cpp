523a4d4649c860df8354aa7187b08b2adcf95e6e
Mihaela Brodetchi <58052769+404UserNotFound@users.noreply.github.com>
2020-10-19 11:17:33 +0100

    Lab 1

---
+++ lab1.cpp
@@ -0,0 +1,236 @@
+/* Mihaela Brodetchi
+	C00242687*/
+#include <iostream>
+using namespace std;
+
+
+bool isLeapYear(int year)
+{
+	if (year%4==0) {
+		
+		if (year % 100 == 0) {
+		
+			if (year % 400 == 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return true;
+		}
+	}
+	else {
+		return false;
+	} 
+
+}
+
+int Reversed(int testNumber)
+{
+	int reverseNum = 0;
+	while (testNumber > 0) {
+		reverseNum = reverseNum * 10 + testNumber % 10;
+		testNumber = testNumber / 10;
+		}
+	return reverseNum;
+}
+bool isAPalindrome(int testNumber)
+{
+	if (Reversed(testNumber) == testNumber) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool isAPrimeNumber(int numbertoTest)
+{
+	if(numbertoTest <= 1) {
+		return false;
+	}
+
+	for (int i = 2; i < numbertoTest; i++)
+	{
+		if (numbertoTest % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+		
+}
+
+int input5CharsConvertToInt()
+{
+	int returnInt = 0;
+	char inputChar;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> inputChar;
+		inputChar = int(inputChar);
+		if (inputChar >=48 && inputChar <= 57) {
+			returnInt = int(inputChar);
+		}
+		else {
+			return 0;
+		}
+	}
+	return returnInt;
+}
+
+int convertBinarytoDecimal(int binaryNumber)
+{
+	int decimal = 0;
+	int base = 1;
+	int remainder = 0;
+
+		while (binaryNumber != 0) {
+			remainder = binaryNumber % 10;
+			decimal = decimal + remainder * base;
+			base = base * 2;
+			binaryNumber = binaryNumber / 10;
+		}
+	return decimal;
+}
+void drawRightAngledTriangle()
+{
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < i + 1; j++)
+			{
+				cout << "A";
+			}
+			cout << "\n";
+		}
+}
+void drawIsocelesTriangle()
+{
+
+}
+void drawIsocelesTriangle2()
+{
+
+}
+// changing anything below here equals 0 marks!!!!!!!!!
+
+int main()
+{
+	std::cout << "Leapyear" << std::endl;
+	if (!isLeapYear(1700))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (isLeapYear(1600))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (isLeapYear(2016))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	std::cout << "NumberReversed" << std::endl;
+	if (Reversed(123) == 321)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+
+	if (Reversed(1) == 1)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (Reversed(1234) != 321)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+
+	std::cout << "Palindrome" << std::endl;
+	if (isAPalindrome(1))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (isAPalindrome(121))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (!isAPalindrome(1213))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	std::cout << "Prime Number" << std::endl;
+	if (isAPrimeNumber(3))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (isAPrimeNumber(2147483647))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (!isAPrimeNumber(4))
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	std::cout << "Binary to Decimal" << std::endl;
+	if (convertBinarytoDecimal(110) == 6)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	if (convertBinarytoDecimal(111) == 7)
+	{
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+	std::cout << "5 chars to int" << std::endl;
+	int convertedInt = input5CharsConvertToInt();
+	std::cout << convertedInt << std::endl;
+
+	std::cout << "Triangles" << std::endl;
+	drawRightAngledTriangle();
+	drawIsocelesTriangle();
+	std::cout << "Triangle Extra" << std::endl;
+	drawIsocelesTriangle2();
+
+}
